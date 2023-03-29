28a4ebac0d6114562ce4e5cfe603f253ebfe2485
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-03-29 20:55:12 +0900

    :art: 코드 공백 수정
    아마 이게 문제라는거 같은데...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,18 +4,18 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
-
 }
+
 rootProject.name = "Idea-Archive-Android"
-include (":presentation")
-include (":di")
-include (":data")
-include (":domain")
+include(":presentation")
+include(":di")
+include(":data")
+include(":domain")

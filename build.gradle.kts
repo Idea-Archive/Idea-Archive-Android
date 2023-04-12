c350f159b052b8fb4251b2589c77b915b0b2e93c
Chaejongin12 <s22055@gsm.hs.kr>
2023-04-12 15:17:11 +0900

    sexy songin

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+
+    dependencies {
+        classpath(Dependency.GradlePlugin.GRADLE_ANDROID)
+        classpath(Dependency.GradlePlugin.GRADLE_KOTLIN)
+        classpath(Dependency.GradlePlugin.GRADLE_HILT)
+        classpath(Dependency.GradlePlugin.GRADLE_KTLINT)
+    }
+}
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+}
+
+apply(plugin = "org.jmailen.kotlinter")
+
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ presentation/src/main/java/com/team_ia/idea_archive_android/IAApplication.kt
@@ -0,0 +1,7 @@
+package com.team_ia.idea_archive_android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IAApplication: Application()

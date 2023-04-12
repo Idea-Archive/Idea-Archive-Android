ecac362b96591c1b6ce2c04532707c732ad08219
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-04-12 20:23:05 +0900

    :memo: Api -> API로 통일

--- data/src/main/java/com/team_ia/data/remote/datasource/member/MemberDataSourceImpl.kt
+++ data/src/main/java/com/team_ia/data/remote/datasource/member/MemberDataSourceImpl.kt
@@ -4,6 +4,6 @@ import com.team_ia.data.remote.api.MemberAPI
 import javax.inject.Inject
 
 class MemberDataSourceImpl @Inject constructor(
-    private val memberApi: MemberAPI
+    private val memberAPI: MemberAPI
 ) : MemberDataSource {
 }

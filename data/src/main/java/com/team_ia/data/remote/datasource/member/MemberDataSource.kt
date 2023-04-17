84e4094e62a3ef3088fb838becbe365a9322f5d2
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-04-18 01:30:34 +0900

    :fire: 삭제한 내용 import 됨
    WithdrawalMemberRequest 없앴는데 실수로 import된거 지우는거 깜빡하고 커밋함

--- data/src/main/java/com/team_ia/data/remote/datasource/member/MemberDataSource.kt
+++ data/src/main/java/com/team_ia/data/remote/datasource/member/MemberDataSource.kt
@@ -1,7 +1,6 @@
 package com.team_ia.data.remote.datasource.member
 
 import com.team_ia.data.remote.request.member.ChangePasswordRequest
-import com.team_ia.data.remote.request.member.WithdrawalMemberRequest
 import com.team_ia.data.remote.response.member.MemberResponse
 
 interface MemberDataSource {

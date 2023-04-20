7761da39e031e1bde9d31b6c2917c7c9a3eb5d01
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-04-20 20:08:04 +0900

    Revert ":memo: shortToast, longToast 함수 추가"
    This reverts commit 41fbfd097aa13a993058cbcd0790ef7823615065.

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/base/BaseActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/base/BaseActivity.kt
@@ -1,7 +1,6 @@
 package com.team_ia.idea_archive_android.ui.base
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -22,12 +21,4 @@ abstract class BaseActivity<B: ViewDataBinding>(
     }
 
     abstract fun createView()
-
-    protected fun shortToast(msg: String){
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-    }
-
-    protected fun longToast(msg: String){
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-    }
 }
--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/base/BaseFragment.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/base/BaseFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -30,12 +29,4 @@ abstract class BaseFragment<B: ViewDataBinding>(
     }
 
     abstract fun createView()
-
-    protected fun shortToast(msg: String){
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
-    }
-
-    protected fun longToast(msg: String){
-        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
-    }
 }

ed2af80624e2e95e9a539c0d8c1c86198f582d97
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-04-25 16:49:09 +0900

    📝usecase에 추가 안한 부분 추가

--- domain/src/main/java/com/team_ia/domain/usecase/auth/SignupUseCase.kt
+++ domain/src/main/java/com/team_ia/domain/usecase/auth/SignupUseCase.kt
@@ -1,9 +1,13 @@
 package com.team_ia.domain.usecase.auth
 
+import com.team_ia.domain.param.SignupParam
 import com.team_ia.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class SignupUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
+    suspend operator fun invoke(signupParam: SignupParam) = kotlin.runCatching {
+        authRepository.signup(signupParam)
+    }
 }

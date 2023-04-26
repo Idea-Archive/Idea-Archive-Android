6010969799f9f6acc4f81d49408d5dc104ebe042
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-04-26 17:00:17 +0900

    :memo: PostNotice API 작성

--- data/src/main/java/com/team_ia/data/remote/datasource/admin/AdminDataSource.kt
+++ data/src/main/java/com/team_ia/data/remote/datasource/admin/AdminDataSource.kt
@@ -1,4 +1,7 @@
 package com.team_ia.data.remote.datasource.admin
 
+import com.team_ia.data.remote.request.admin.PostNoticeRequest
+
 interface AdminDataSource {
+    suspend fun postNotice(postNoticeRequest: PostNoticeRequest)
 }

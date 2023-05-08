362cd874e15fad10e0f62e1a908ce7ec8dc06c18
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-05-08 16:15:53 +0900

    :memo: Write out ChangeProfilePicture API

--- data/src/main/java/com/team_ia/data/remote/datasource/img/ImgDataSource.kt
+++ data/src/main/java/com/team_ia/data/remote/datasource/img/ImgDataSource.kt
@@ -1,7 +1,9 @@
 package com.team_ia.data.remote.datasource.img
 
+import com.team_ia.data.remote.request.img.ChangeProfilePictureRequest
 import com.team_ia.data.remote.request.img.PostProfilePictureRequest
 
 interface ImgDataSource {
     suspend fun postProfilePicture(postProfilePictureRequest: PostProfilePictureRequest)
+    suspend fun changeProfilePicture(changeProfilePictureRequest: ChangeProfilePictureRequest)
 }
--- data/src/main/java/com/team_ia/data/remote/datasource/img/ImgDataSourceImpl.kt
+++ data/src/main/java/com/team_ia/data/remote/datasource/img/ImgDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.team_ia.data.remote.datasource.img
 
 import com.team_ia.data.remote.api.ImgAPI
+import com.team_ia.data.remote.request.img.ChangeProfilePictureRequest
 import com.team_ia.data.remote.request.img.PostProfilePictureRequest
 import com.team_ia.data.utils.IAApiHandler
 import javax.inject.Inject
@@ -12,6 +13,12 @@ class ImgDataSourceImpl @Inject constructor(
     override suspend fun postProfilePicture(postProfilePictureRequest: PostProfilePictureRequest) {
         return IAApiHandler<Unit>()
             .httpRequest { imgAPI.postProfilePicture(postProfilePictureRequest) }
+            .sendRequest()
+    }
+
+    override suspend fun changeProfilePicture(changeProfilePictureRequest: ChangeProfilePictureRequest) {
+        return IAApiHandler<Unit>()
+            .httpRequest { imgAPI.changeProfilePicture(changeProfilePictureRequest) }
             .sendRequest()
     }
 }

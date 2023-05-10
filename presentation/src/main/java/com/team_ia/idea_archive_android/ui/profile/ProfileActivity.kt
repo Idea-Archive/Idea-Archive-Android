34ff86cd59233c546b8a9d1c6d8bfb4d78f85aa8
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-05-10 23:36:57 +0900

    :memo: add ProfileActivity

---
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.team_ia.idea_archive_android.ui.profile
+
+import com.team_ia.idea_archive_android.R
+import com.team_ia.idea_archive_android.databinding.ActivityProfileBinding
+import com.team_ia.idea_archive_android.ui.base.BaseActivity
+
+class ProfileActivity : BaseActivity<ActivityProfileBinding>(R.layout.activity_profile) {
+
+    override fun createView() {
+
+    }
+}

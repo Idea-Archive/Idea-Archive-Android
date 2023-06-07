f02562d502e784bf60c4b55f7b66a6f4a5124ad5
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-06-07 21:02:41 +0900

    :memo: change SendVerificationUseCase -> SendVerificationCodeUseCase

--- domain/src/main/java/com/team_ia/domain/usecase/email/SendVerificationCodeUseCase.kt
+++ domain/src/main/java/com/team_ia/domain/usecase/email/SendVerificationCodeUseCase.kt
@@ -4,7 +4,7 @@ import com.team_ia.domain.param.SendVerificationCodeParam
 import com.team_ia.domain.repository.EmailRepository
 import javax.inject.Inject
 
-class SendVerificationUseCase @Inject constructor(
+class SendVerificationCodeUseCase @Inject constructor(
     private val emailRepository: EmailRepository
 ) {
     suspend operator fun invoke(sendVerificationCodeParam: SendVerificationCodeParam) = kotlin.runCatching {

4d9b859d0629050475702f6c57e0dbe3970e1734
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-06-21 20:34:32 +0900

    extension 함수 추가

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/LoginActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/LoginActivity.kt
@@ -1,5 +1,37 @@
 package com.team_ia.idea_archive_android.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
-class LoginActivity : AppCompatActivity(){
+import android.view.View
+import com.team_ia.idea_archive_android.R
+import com.team_ia.idea_archive_android.databinding.ActivityLoginPageBinding
+import com.team_ia.idea_archive_android.ui.base.BaseActivity
+import com.team_ia.idea_archive_android.utils.extension.changeActivatedWithEnabled
+import com.team_ia.idea_archive_android.utils.extension.setOnTextChanged
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : BaseActivity<ActivityLoginPageBinding>(R.layout.activity_login_page ){
+    override fun createView() {
+        binding.login
+        initView()
+    }
+
+    private fun initView() = binding.apply {
+        etEmail.run {
+            setOnTextChanged { p0, _, _, _ ->
+                btnLogin.changeActivatedWithEnabled(!p0.isNullOrBlank()&& !binding.etPassword.text.isNullOrBlank())
+            }
+            setOnFocusChangeListener{_,b ->
+                etEmail.isActivated = b
+            }
+        }
+        etPassword.run {
+            setOnTextChanged { p0, _, _, _ ->
+                btnLogin.changeActivatedWithEnabled(!p0.isNullOrBlank()&& !binding.etEmail.text.isNullOrBlank())
+            }
+            setOnFocusChangeListener{_,b ->
+                etPassword.isActivated = b
+            }
+        }
+    }
+
 }

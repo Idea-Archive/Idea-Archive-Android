8dd1da1c13799502b4e12ed2e758c7e4325df8e6
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-06-29 10:55:29 +0900

    ci 문제 해결

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/LoginActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/LoginActivity.kt
@@ -98,7 +98,7 @@ class LoginActivity : BaseActivity<ActivityLoginPageBinding>(R.layout.activity_l
         }
     }
 
-  private fun onClick(view: View){
+  fun onClick(view: View){
       when(view){
           binding.ibtnBackButton -> {
               finish()

9a3db66b9b4c4f48b40a5269641ef5b314a2d164
Chaejongin12 <s22055@gsm.hs.kr>
2023-07-03 22:44:55 +0900

    Google에서 kakao로 긴급하게 넘어가기전 일단 커밋

---
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/viewmodel/GoogleSocialLoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.team_ia.idea_archive_android.ui.viewmodel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GoogleSocialLoginViewModel @Inject constructor(
+
+){
+}

85928ca937cad98ff19f3dcd2cdb693e3a515e31
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-07-03 14:15:09 +0900

    :memo: Complete my page

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/profile/PostItem.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/profile/PostItem.kt
@@ -1,12 +1,28 @@
 package com.team_ia.idea_archive_android.ui.profile
 
+import androidx.activity.viewModels
+import com.team_ia.domain.model.PostModel
 import com.team_ia.idea_archive_android.R
+import com.team_ia.idea_archive_android.adapter.CategoryListAdapter
 import com.team_ia.idea_archive_android.databinding.ItemPostBinding
 import com.team_ia.idea_archive_android.ui.base.BaseActivity
+import com.team_ia.idea_archive_android.ui.viewmodel.MyViewModel
 
-class PostItem : BaseActivity<ItemPostBinding>(R.layout.item_post) {
+class PostItem(private val data: PostModel) : BaseActivity<ItemPostBinding>(R.layout.item_post) {
+    private lateinit var adapter: CategoryListAdapter
+    private val viewModel by viewModels<MyViewModel>()
+
 
     override fun createView() {
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+
+    fun initRecycler() {
+        adapter = CategoryListAdapter(viewModel.postData.value)
+        binding.rvCategory.adapter = adapter
     }
 
 }

3b3ff0ba7dd4352f2425624bdb824300b6581a78
Chaejongin12 <s22055@gsm.hs.kr>
2023-07-10 02:02:21 +0900

    Activity, Fragment 생성

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainActivity.kt
@@ -3,11 +3,24 @@ package com.team_ia.idea_archive_android.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.team_ia.idea_archive_android.R
+import com.team_ia.idea_archive_android.databinding.ActivityMainPageBinding
+import com.team_ia.idea_archive_android.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainPageBinding>(R.layout.activity_main_page) {
+
+    val majorFilterList = listOf("FrontEnd", "BackEnd", "Android", "iOS", "Cloud", "GameDevelop", "MachineLearning", "Embedded", "DBA", "WebPublisher", "Design")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun createView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
     }
 
 }
---
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainEntireFragment.kt
@@ -0,0 +1,17 @@
+package com.team_ia.idea_archive_android.ui.main
+
+import com.team_ia.idea_archive_android.R
+import com.team_ia.idea_archive_android.databinding.FragmentMainEntireBinding
+import com.team_ia.idea_archive_android.ui.base.BaseFragment
+
+class MainEntireFragment : BaseFragment<FragmentMainEntireBinding>(R.layout.fragment_main_entire) {
+    override fun createView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+
+
+}

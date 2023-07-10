f04a4ab31295b4be726d5a34fb61d4c8e34cf8af
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-07-10 14:15:40 +0900

    기능 테스트 전 커밋

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/AuthCodeInputActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/AuthCodeInputActivity.kt
@@ -1,18 +1,90 @@
 package com.team_ia.idea_archive_android.ui.main
 
+import android.content.Intent
+import android.view.View
+import androidx.activity.viewModels
 import com.team_ia.idea_archive_android.R
 import com.team_ia.idea_archive_android.databinding.ActivityAuthCodeInputPageBinding
 import com.team_ia.idea_archive_android.ui.base.BaseActivity
+import com.team_ia.idea_archive_android.ui.viewmodel.SignupViewModel
+import com.team_ia.idea_archive_android.utils.Event
+import com.team_ia.idea_archive_android.utils.extension.changeAtivatedWithEnabled
+import com.team_ia.idea_archive_android.utils.extension.setOnTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.sign
 
 @AndroidEntryPoint
 class AuthCodeInputActivity: BaseActivity<ActivityAuthCodeInputPageBinding>(R.layout.activity_auth_code_input_page) {
+    private val signupViewModel by viewModels<SignupViewModel>()
     override fun createView() {
+        binding.authCodeInput = this
 
+    }
+    fun initView() = binding.apply {
+        etInputAuthCode1.run {
+            setOnTextChanged { p0, _, _, _ ->
+                btnCheck.changeAtivatedWithEnabled(p0.isNullOrBlank()
+                        && etInputAuthCode2.text.isNullOrBlank()
+                        && etInputAuthCode3.text.isNullOrBlank()
+                        && etInputAuthCode4.text.isNullOrBlank())
+
+            }
+        }
+    }
+
+    fun onClick(view: View){
+        when(view){
+            binding.btnCheck -> {
+                val email = signupViewModel.emailData.value!!
+                val authKey = binding.etInputAuthCode1.text.toString() +
+                        binding.etInputAuthCode2.text.toString() +
+                        binding.etInputAuthCode3.text.toString() +
+                        binding.etInputAuthCode4.text.toString()
+                if (binding.etInputAuthCode1.text.isNullOrBlank()
+                    && binding.etInputAuthCode2.text.isNullOrBlank()
+                    && binding.etInputAuthCode3.text.isNullOrBlank()
+                    && binding.etInputAuthCode4.text.isNullOrBlank()){
+                    signupViewModel.authCodeCheck(email = email, authKey = authKey.toInt())
+                }
+            }
+        }
     }
 
     override fun observeEvent() {
-        
+        observeSignUp()
+    }
+
+    private fun observeSignUp(){
+        val email = signupViewModel.emailData.value!!
+        val password = signupViewModel.passwordData.value!!
+        val name = signupViewModel.nameData.value!!
+        signupViewModel.signupInfo.observe(this){
+            when (it){
+                Event.Success -> {
+                    signupViewModel.signup(email, password, name)
+                    when (it){
+                        Event.Success -> {
+                            startActivity(Intent(this, AuthenticationSuccessActivity::class.java))
+                            finish()
+                        }
+                        Event.Unauthorized -> {
+                            startActivity(Intent(this, AuthenticationFailedActivity::class.java))
+                            finish()
+                        }
+                        else -> {}
+                    }
+                }
+                Event.BadRequest -> {
+                    shortToast("인증번호가 일치하지 않습니다.")
+                }
+                Event.Unauthorized ->{
+                    shortToast("인증 시간이 만료되었어요.")
+                    startActivity(Intent(this, SignUpActivity::class.java))
+                    finish()
+                }
+                else -> {}
+            }
+        }
     }
 
 }

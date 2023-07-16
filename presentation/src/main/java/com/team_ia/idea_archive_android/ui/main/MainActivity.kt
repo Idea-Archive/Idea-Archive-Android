3c6d09a723884fb3baa521fb6d54ea37c1763277
Chaejongin12 <s22055@gsm.hs.kr>
2023-07-16 23:09:20 +0900

    :memo: :: fragment main entire로 replace 세팅
    :: fragment main entire로 replace 세팅

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainActivity.kt
@@ -2,20 +2,32 @@ package com.team_ia.idea_archive_android.ui.main
 
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import com.team_ia.idea_archive_android.R
 import com.team_ia.idea_archive_android.databinding.ActivityMainPageBinding
 import com.team_ia.idea_archive_android.ui.base.BaseActivity
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 class MainActivity : BaseActivity<ActivityMainPageBinding>(R.layout.activity_main_page) {
+
+    val fragmentManager: FragmentManager = supportFragmentManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
         val entirePost = findViewById<TextView>(R.id.tv_entire)
+
         entirePost.setOnClickListener {
-            val entirePostFragment = MainEntireFragment()
+            showEntirePostFragment()
         }
+    }
+
+
+    private fun showEntirePostFragment() {
+        fragmentManager.beginTransaction()
+            .replace(R.id.fcv_main_entire_fragment_container, MainEntireFragment())
     }
 
     override fun createView() {
--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainEntireFragment.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainEntireFragment.kt
@@ -2,10 +2,13 @@ package com.team_ia.idea_archive_android.ui.main
 
 import com.team_ia.idea_archive_android.R
 import com.team_ia.idea_archive_android.adapter.MajorFilterListAdapter
+import com.team_ia.idea_archive_android.adapter.PostListAdapter
 import com.team_ia.idea_archive_android.databinding.FragmentMainEntireBinding
 import com.team_ia.idea_archive_android.ui.base.BaseFragment
 
 class MainEntireFragment : BaseFragment<FragmentMainEntireBinding>(R.layout.fragment_main_entire) {
+
+    private lateinit var adapter: PostListAdapter
 
     private lateinit var majorFilterListAdapter: MajorFilterListAdapter
     val majorFilterList = listOf("FrontEnd", "BackEnd", "Android", "iOS", "Cloud", "GameDevelop", "MachineLearning", "Embedded","DevOps", "DBA", "WebPublisher", "Design")

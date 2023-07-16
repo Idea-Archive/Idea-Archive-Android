590dbeb4b572e45e1886f632d86912f08c6478d1
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-07-17 03:01:51 +0900

    signup 기능 완성
    내일 아침에 기능 테스트 하고 말씀드리면 리뷰 해주세요

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/SignUpFragment.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/SignUpFragment.kt
@@ -13,7 +13,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignUpFragment : BaseFragment<FragmentSignUpPageBinding>(R.layout.fragment_sign_up_page) {
-
     private val signupViewModel by activityViewModels<SignupViewModel>()
     override fun createView() {
         binding.signupfragment = SignUpFragment()

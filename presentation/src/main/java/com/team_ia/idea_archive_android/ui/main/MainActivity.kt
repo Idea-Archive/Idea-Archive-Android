b6046758d7b5c4bcf9227b3de0f92d6f0b52bf40
Chaejongin12 <s22055@gsm.hs.kr>
2023-07-27 14:31:00 +0900

    :memo: :: Navigation Drawer xml script와Activity  code 연결
    :: Navigation Drawer xml script와Activity  code 연결

--- presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/team_ia/idea_archive_android/ui/main/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.team_ia.idea_archive_android.ui.main
 
+import android.content.Intent
+import android.provider.ContactsContract.Profile
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
 import com.team_ia.idea_archive_android.R
 import com.team_ia.idea_archive_android.databinding.ActivityMainPageBinding
 import com.team_ia.idea_archive_android.ui.base.BaseActivity
+import com.team_ia.idea_archive_android.ui.profile.ProfileActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainPageBinding>(R.layout.activity_main_page) {
 
-    val fragmentManager: FragmentManager = supportFragmentManager
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private val fragmentManager: FragmentManager = supportFragmentManager
     private var isFabOpen = false
 
     override fun createView() {
@@ -65,6 +72,48 @@ class MainActivity : BaseActivity<ActivityMainPageBinding>(R.layout.activity_mai
         binding.fbtnWritePost.animate().translationY(0f)
         binding.fbtnWriteNotice.animate().translationY(0f)
         isFabOpen = false
+    }
+
+    private fun setUpViews() {
+        drawerLayout = binding.mainPageDrawerLayout
+        navigationView = binding.nvMainNavigation
+
+        binding.btnMainPageMenuBarButton.setOnClickListener {
+            drawerLayout.openDrawer(navigationView)
+        }
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+//                R.id.item_notice -> {
+//                      // 공지부분 퍼블리싱 후 연결
+//                }
+
+                R.id.item_view_my_post -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.item_view_like_post -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+//                R.id.item_logout -> {
+//                   // 대진님 여기다가 로그아웃 작업 하시면 됩니다.
+//                }
+            }
+            drawerLayout.closeDrawer(navigationView)
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(navigationView)) {
+            drawerLayout.closeDrawer(navigationView)
+        } else {
+            super.getOnBackPressedDispatcher()
+        }
     }
     override fun observeEvent() {
     }
